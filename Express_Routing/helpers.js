4ddb94f4f2658e7c5999b7aa334e449536767dd1
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-01-16 19:50:23 -0800

    Added some functions to helpers.js

--- Express_Routing/helpers.js
+++ Express_Routing/helpers.js
@@ -1,3 +1,10 @@
+function createFrequencyCounter(arr) {
+    return arr.reduce(function(acc, next) {
+        acc[next] = (acc[next] || 0) + 1;
+        return acc;
+    }, {});
+}
+
 function findMean(nums) {
     let total = 0;
     for (let i = 0; i < nums.length; i++) {
@@ -16,12 +23,41 @@ function findMedian(nums) {
         median = nums([numLength - 1] / 2);
     }
     return median;
-
 }
 
-function findMode(nums) {
+// function findMode(arr) {
+//     let count = [];
+//     let freqCount = 0;
+//     let mode = [];
+//     for (let i in arr) {
+//         count[arr[i]] = (count[arr[i] || 0]) + 1;
+//         if (count[arr[i]] > freqCount) {
+//             freqCount = count[arr[i]];
+//         }
+//     }
+//     for (let j in count) {
+//         if (count[j] === freqCount) {
+//             mode.push(j);
+//         }
+//     }
+//     return mode;
+// }
 
+function findMode(arr) {
+    let freqCounter = createFrequencyCounter(arr);
+    let count = 0;
+    let mostFrequent;
+
+    for (let key in freqCounter) {
+        if (freqCounter[key] > count) {
+            mostFrequent = key;
+            count = freqCounter[key];
+        }
+    }
+    return +mostFrequent;
 }
+
+
 module.exports = {
     findMean,
     findMedian,
