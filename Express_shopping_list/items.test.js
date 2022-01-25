248621745635f8d1d781529bfa3c054283c0b741
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-01-24 22:04:29 -0800

    Added test

--- Express_shopping_list/items.test.js
+++ Express_shopping_list/items.test.js
@@ -3,7 +3,7 @@ process.env.NODE_ENV = "test";
 const request = require("supertest");
 
 const app = require("../app");
-let items = require("./fakeDb");
+const items = require("./fakeDb");
 
 let candy = { name: "Gummy Bears", price: 1.99 };
 
@@ -14,16 +14,31 @@ afterEach(function() {
     items.length = 0;
 });
 
-describe("GET /", () => {
-    test("Get all items", async() => {
+describe("GET /items", async function() {
+    test("Get all items", async function() {
         const res = await request(app).get("/");
         // expect(res.statusCode).toBe(200);
-        expect(res.body).toEqual({ items: [candy] })
-            // expect(res.body).toEqual({ items: [{ name: "Gummy Bears", price: 1.99 }] })
+        expect(res.body).toEqual({ items: [candy] });
+        // expect(res.body).toEqual({ items: [{ name: "Gummy Bears", price: 1.99 }] })
+        //debugger;
     })
 })
 
-describe("POST /", () => {
+describe("GET /items/:name", () => {
+    test("Get item by name", async() => {
+        const res = await request(app).get(`/items/${candy.name}`);
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({ item: candy })
+    })
+    test("Responds with 404 for invalid item", async() => {
+        const res = await request(app).get(`/items/Umaibo`);
+        expect(res.statusCode).toBe(404);
+        expect(res.body).toEqual({ item: candy })
+    })
+})
+
+
+describe("POST /items", () => {
     test("Create an item", async() => {
         const res = await request(app).post("/").send({ name: "Bubblegum", price: 0.99 });
         expect(res.statusCode).toBe(201);
@@ -31,7 +46,7 @@ describe("POST /", () => {
     })
 })
 
-describe("PATCH /:name", () => {
+describe("PATCH /items/:name", () => {
     test("Updates a single item", async() => {
         const res = await request(app).patch(`/items/${candy.name}`).send({ name: "Dubble Bubble", price: 1.29 });
         expect(res.statusCode).toBe(200);
